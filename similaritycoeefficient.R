479dfecabfa967d0b326ddbacff5defb3fbf191f
Abhishek Singh <49085953+abhishekasingh@users.noreply.github.com>
2020-03-25 14:32:16 -0700

    Add files via upload

--- similaritycoeefficient.R
+++ similaritycoeefficient.R
@@ -1,25 +1,21 @@
 # Similarity Coefficient
 
 ## Import Library
-list.of.packages <- c("MASS", "philentropy","dplyr","tidyverse", "tablerDash","ggpol","XML")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 library(MASS)
 library(philentropy)
 library(dplyr)
 library(tidyverse)
-library(ggpol)
 
 ## Importing the dataset
-social.data = read.csv('dataset/maindataset/Social_Network_Ads.csv')
+social.data = read.csv('dataset/Social_Network_Ads.csv')
 
-
+## Rename the columns
 colnames(social.data)
 colnames(social.data) <- tolower(make.names(colnames(social.data)))
 colnames(social.data)
 
 ## Summary of the data
+
 summary(social.data)
 
 ## Explanatory data analysis
@@ -36,7 +32,7 @@ gender.plt <- social.data %>%
     ylab("Counts") +
     theme_minimal() +
     theme(plot.title = element_text(hjust = 0.5))
-    
+
 print(gender.plt)
 
 ### Age
@@ -73,14 +69,6 @@ age.plt <- ggplot(data=social.data.age, aes(x=as.factor(age.band),fill=gender)) 
 print(age.plt)
 
 ## Converting into binary values
-
-social.data <- social.data %>%
-    select(gender, age, estimatedsalary,purchased) %>% 
-    mutate(gender = ifelse(social.data$gender == "Female", 0, 1) ) %>%
-    mutate(age = ifelse(social.data$age <= 37, 1, 0) ) %>%
-    mutate(gender = ifelse(social.data$estimatedsalary <= 70000, 1, 0) )
-    
-
 social.data$gender <- ifelse(social.data$gender == "Female", 0, 1)
 social.data$age <- ifelse(social.data$age <= 37, 1, 0)
 social.data$estimatedsalary <- ifelse(social.data$estimatedsalary <= 70000, 1, 0)
@@ -88,19 +76,62 @@ social.data$estimatedsalary <- ifelse(social.data$estimatedsalary <= 70000, 1, 0
 ## Structure of data
 str(social.data)
 
-social.data <- social.data[sample(nrow(social.data), 10), 2:length(social.data)]
+## Sample 10 oberservation from the dataset
+##social.data <- social.data[sample(nrow(social.data), 10), 2:length(social.data)]
 
 ## Check the data
 head(social.data)
 
 ## Simple Matching
-simple.match.data <- 1- distance(social.data,method = 'gower')
-simple.match.data
+simple.matching <- 1- distance(social.data,method = 'gower')
+round(as.matrix(simple.matching)[1:5, 1:5], 1)
+
+fviz_nbclust(simple.matching, kmeans, method = "wss") +
+    geom_vline(xintercept = 3, linetype = 2)
+
+sm.knn.res <- kmeans(simple.matching, 3, nstart = 50)
+sm.knn.res
+
+fviz_cluster(sm.knn.res, data = simple.matching,
+             palette = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"), 
+             ellipse.type = "euclid", # Concentration ellipse
+             star.plot = TRUE, # Add segments from centroids to items
+             repel = TRUE, # Avoid label overplotting (slow)
+             ggtheme = theme_minimal()
+             )
 
 ## Jaccard 
-jaccard.data <- distance(social.data,method = 'jaccard')
-jaccard.data
+jaccard.res <- distance(social.data,method = 'jaccard')
+jaccard.res
+
+fviz_nbclust(jaccard.res, kmeans, method = "wss") +
+    geom_vline(xintercept = 3, linetype = 2)
+
+jaccard.knn.res <- kmeans(jaccard.res, 3, nstart = 50)
+jaccard.knn.res
+
+fviz_cluster(jaccard.knn.res, data = jaccard.res,
+             palette = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"), 
+             ellipse.type = "euclid", # Concentration ellipse
+             star.plot = TRUE, # Add segments from centroids to items
+             repel = TRUE, # Avoid label overplotting (slow)
+             ggtheme = theme_minimal()
+)
 
 ## Dice
-dice.data <- distance(social.data,method = 'dice')
-dice.data
+dice.res <- distance(social.data,method = 'dice')
+dice.res
+
+fviz_nbclust(dice.res, kmeans, method = "wss") +
+    geom_vline(xintercept = 3, linetype = 2)
+
+dice.knn.res <- kmeans(dice.res, 3, nstart = 50)
+dice.knn.res
+
+fviz_cluster(dice.knn.res, data = dice.res,
+             palette = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07"), 
+             ellipse.type = "euclid", # Concentration ellipse
+             star.plot = TRUE, # Add segments from centroids to items
+             repel = TRUE, # Avoid label overplotting (slow)
+             ggtheme = theme_minimal()
+)
